d427d23fa8ccdf21830a7ee82f8e3ca914b6956c
EstefanoPacheco <a00835025@tec.com>
2021-08-29 16:56:32 -0500

    29082021

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -4,7 +4,34 @@ def main():
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
     
-    print("Hola")
+    if z < x > y:
+        if y > z:
+            print(z)
+            print(y)
+            print(x)
+        else: 
+            print(y)
+            print(z)
+            print(x)
+    elif x < y > z:
+        if x > z:
+            print(z)
+            print(x)
+            print(y)
+        else: 
+            print(x)
+            print(z)
+            print(y)
+    else:
+        if x > y:
+            print(y)
+            print(x)
+            print(z)
+        else:
+            print(x)
+            print(y)
+            print(z)
+
 
 if __name__=='__main__':
     main()
