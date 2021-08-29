5a9f6f62dff9fe924c39e99ce5fe82ca686e93c1
EstefanoPacheco <a00835025@tec.com>
2021-08-29 16:43:03 -0500

    29082021

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,6 +1,15 @@
 
 def main():
-    pass
+    edad = int(input("Ingresa tu edad: "))
+
+    if edad >= 18: 
+        identificacion = input("¿Tiene identificación oficial? (s/n): ")
+        if identificacion == "s":
+            print("Trámite de licencia concedido")
+        elif identificacion == "n":
+            print("No cumples requisitos")
+        else: "Respuesta incorrecta"
+    else: print("No cumples requisitos")
 
 
 if __name__ == '__main__':
